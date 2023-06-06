b57e601c8b2059ce7903c4da022d6627898d7543
monjas99 <monjasandreu@gmail.com>
2023-06-06 08:25:24 +0200

    1!

--- src/pipex.c
+++ src/pipex.c
@@ -26,9 +26,9 @@ int	main(int ac, char **av)
 		ft_printf("Error: %s\n", strerror(errno)); 
 		perror("Error al abrir el archivo");
 	}
-	outfile = open(av[4], O_TRUNC | O_CREAT | O_RDWR);
-	if (outfile < 0)
-		return (0);
+	//outfile = open(av[4], O_TRUNC | O_CREAT | O_RDWR);
+	//if (outfile < 0)
+	//	return (0);
 	
 
 	return (0);
