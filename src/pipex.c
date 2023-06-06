3958e2ada2186410b2ac57d07472c71eb077984a
David Monjas Andreu <dmonjas-@c1r15s2.42madrid.com>
2023-06-06 08:24:15 +0200

    pipex

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: dmonjas- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 12:22:05 by dmonjas-          #+#    #+#             */
-/*   Updated: 2023/06/05 11:03:16 by dmonjas-         ###   ########.fr       */
+/*   Updated: 2023/06/06 08:24:03 by dmonjas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,9 @@ int	main(int ac, char **av)
 		ft_printf("Error: %s\n", strerror(errno)); 
 		perror("Error al abrir el archivo");
 	}
-//	outfile = open(av[4], O_TRUNC | O_CREAT | O_RDWR);
-//	if (outfile < 0)
-//		return (0);
+	outfile = open(av[4], O_TRUNC | O_CREAT | O_RDWR);
+	if (outfile < 0)
+		return (0);
 	
 
 	return (0);
